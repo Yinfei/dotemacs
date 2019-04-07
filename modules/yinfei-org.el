d56029a69685bd7100a1581961e4a0026e4113bc
Yinfei <yinfei84@gmail.com>
2019-04-07 23:53:58 +0200

    Add org-mode

---
+++ modules/yinfei-org.el
@@ -0,0 +1,42 @@
+;;; yinfei-org.el --- Org-mode config
+
+;; Copyright (C) 2019 Yohan Piron
+
+;; Author: Yohan Piron <yinfei84@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; Code:
+
+(require 'org)
+(require 'french-holidays)
+
+(setq org-directory "~/Dropbox/Org/")
+(setq org-agenda-files (list "~/Dropbox/Org/Agenda/Perso.org"
+                             "~/Dropbox/Org/Agenda/Pro.org"
+                             "~/Dropbox/Org/Agenda/Birthdays.org"
+                             "~/Dropbox/Org/Agenda/Holidays.org"))
+
+(setq calendar-holidays holiday-french-holidays)
+(setq org-agenda-span 14)
+
+(setq org-ellipsis " ⤵")
+(setq org-log-done 'time)
+(setq org-adapt-indentation nil)
+
+(define-key global-map "\C-a" 'org-agenda)
+
+(provide 'yinfei-org)
+;;; yinfei-org.el ends here
